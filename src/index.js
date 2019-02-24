d0fe387359027f82a0572d729cdad1b1ddb96b01
J.D. Zamfirescu <zamfire@gmail.com>
2019-02-24 09:49:47 -0800

    Adding input mechanism

--- src/index.js
+++ src/index.js
@@ -16,23 +16,43 @@ var pages = {
     content:
       "Welcome aboard the choo-choo train! Please make your way to your seat. What's the number?",
     image: "train.png",
-    label1: "12E",
-    label2: "97C",
-    page1: "death",
-    page2: "life"
+    input: {
+      type: "select",
+      values: ["", "12A", "12B", "12C", "11A", "11B", "11C"],
+      saveKey: "seat"
+    },
+    label1: "Next",
+    page1: "next-stop-soweto"
+  },
+  ontheship: {
+    content: "Welcome abord, sir! What is your destination?",
+    input: {
+      type: "text",
+      saveKey: "destination"
+    },
+    label1: "Next",
+    page1: "titanic"
   }
 };
 
 class Page extends Component {
   render() {
     var pageData = pages[this.props.pageName];
+    if (!pageData) {
+      throw new Error("Eek! No page here!");
+    }
+
     var goToPage = this.props.goToPage;
+    var saveUserData = this.props.saveUserData;
 
     function goToPage1() {
       goToPage(pageData.page1);
     }
     function goToPage2() {
       goToPage(pageData.page2);
+    }
+    function handleChange(event) {
+      saveUserData(pageData.input.saveKey, event.target.value);
     }
 
     var image = "";
@@ -51,10 +71,39 @@ class Page extends Component {
     if (pageData.page2) {
       button2 = <button onClick={goToPage2}>{pageData.label2}</button>;
     }
+    var input = "";
+    if (pageData.input) {
+      var inputData = pageData.input;
+      if (inputData.type == "select") {
+        input = (
+          <p>
+            <select
+              value={this.props.userData[inputData.saveKey]}
+              onChange={handleChange}
+            >
+              {inputData.values.map(v => (
+                <option value={v}>{v}</option>
+              ))}
+            </select>
+          </p>
+        );
+      } else if (inputData.type == "text") {
+        input = (
+          <p>
+            <input
+              type="text"
+              value={this.props.userData[inputData.saveKey]}
+              onChange={handleChange}
+            />
+          </p>
+        );
+      }
+    }
 
     return (
       <div>
         <p>{pageData.content}</p>
+        {input}
         {image}
         {button1}
         {button2}
@@ -68,10 +117,12 @@ class App extends Component {
     super(props);
 
     this.state = {
-      page: "start"
+      page: "start",
+      userData: {}
     };
 
     this.goToPage = this.goToPage.bind(this);
+    this.saveUserData = this.saveUserData.bind(this);
   }
 
   goToPage(pageName) {
@@ -80,10 +131,23 @@ class App extends Component {
     });
   }
 
+  saveUserData(key, value) {
+    function updateState(state) {
+      var newState = { userData: { ...state.userData, [key]: value } };
+      return newState;
+    }
+    this.setState(updateState);
+  }
+
   render() {
     return (
       <div className="App">
-        <Page pageName={this.state.page} goToPage={this.goToPage} />
+        <Page
+          pageName={this.state.page}
+          goToPage={this.goToPage}
+          userData={this.state.userData}
+          saveUserData={this.saveUserData}
+        />
       </div>
     );
   }
